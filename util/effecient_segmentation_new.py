af4593a4873ceb3280b48327e972fb5ede4030db
lw0741 <1422851298@qq.com>
2023-04-27 22:58:24 +0800

    Update 'util/effecient_segmentation_new.py'

--- util/effecient_segmentation_new.py
+++ util/effecient_segmentation_new.py
@@ -10,7 +10,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def __proc(img: Tensor) -> Tensor:
     img = img.permute(1, 2, 0).detach().cpu().numpy()
-    label = segmentation.felzenszwalb(img, 1, 0.8, 20)
+    label = segmentation.felzenszwalb(img, 1, 0.8, 60)
     # label = segmentation.slic(
     #     img, compactness=30, n_segments=400, start_label=1)
     # rag = graph.rag_mean_color(img, label)
